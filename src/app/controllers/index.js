f85f80ccf06fb7ea23325d80bdb73f3cbeeda745
Maykon <maykons.df@gmail.com>
2020-02-09 17:14:27 -0300

    .

--- src/app/controllers/index.js
+++ src/app/controllers/index.js
@@ -3,11 +3,13 @@ import SessionController from './SessionController';
 import FileController from './FileController';
 import ProviderController from './ProviderController';
 import AppointmentController from './AppointmentController';
+import ScheduleController from './ScheduleController';
 
 export {
   UserController,
   SessionController,
   FileController,
   ProviderController,
-  AppointmentController
+  AppointmentController,
+  ScheduleController
 };
--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,8 @@ import {
   SessionController,
   FileController,
   ProviderController,
-  AppointmentController
+  AppointmentController,
+  ScheduleController
 } from './app/controllers';
 
 import authMiddleware from './app/middlewares/auth';
@@ -24,7 +25,11 @@ routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
 
+routes.get('/appointments', AppointmentController.index);
+
 routes.post('/appointments', AppointmentController.store);
+
+routes.post('/schedule', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
